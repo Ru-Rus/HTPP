8cb3b66b39e01a9666717171d9ff331e81e77b84
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-11-04 14:07:11 +0800

    getting a GET req to fetch Data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
@@ -11,7 +11,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private htppClient = inject(HttpClient);
+  private destoryRef = inject(DestroyRef);
+  ngOnInit() {
+    const sub = this.htppClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (resData) => {
+          console.log(resData.places);
+        },
+      });
+
+    this.destoryRef.onDestroy(() => {
+      sub.unsubscribe();
+    });
+  }
 }
