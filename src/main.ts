d19319739ea94c292d743a6fc7fb8641ac236058
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-11-11 14:19:16 +0800

    Introducing HTTP Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandler,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // const req = request.clone({
+  //   headers: request.headers.set('X-Debug', 'Testing')
+  // })
+
+  console.log('[Outgoing Req]');
+  console.log(request);
+  return next(request);
+  //  return next(req);
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
-} ).catch((err) => console.error(err));
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
+}).catch((err) => console.error(err));
